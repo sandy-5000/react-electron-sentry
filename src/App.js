bed1f8c1513f5557c25d089610d1baf3032988c3
Sandeep Kumar Bhaviri <bsandeep@corp.untd.com>
2023-11-13 16:04:51 +0530

    upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [name, setName] = useState('Sandy Blaze')
+  const [loop, setLoop] = useState(null)
+
+  useEffect(() => {
+
+    const inter = setInterval(() => {
+      setName(p => {
+        if (p) {
+          return null
+        }
+        return 'Sandy Blaze'
+      })
+    }, 5000)
+
+    setLoop(p => {
+      clearInterval(p)
+      return inter
+    })
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,17 +31,29 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <p>
+            <code>name: {name || "User"}</code>
+          </p>
+          <p>
+            <code>LoopId: {loop}</code>
+          </p>
+          <button
+            style={{
+              padding: '10px',
+              backgroundColor: '#323b4d',
+              border: 0,
+              borderRadius: 10,
+              color: '#61dafb',
+              fontWeight: '500',
+              fontSize: 15
+            }}
+            onClick={() => console.log('length', name.length)}
+          >Crash App</button>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import * as Sentry from "@sentry/electron"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+Sentry.init({
+  dsn: "https://f8d352a34ce4c8550bb82e7b366a3229@o4506206215733248.ingest.sentry.io/4506218088366080",
+  integrations: [
+		new Sentry.BrowserTracing({
+			tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+		}),
+		new Sentry.Replay(),
+	],
+	tracesSampleRate: 1.0,
+	replaysSessionSampleRate: 0.1,
+	replaysOnErrorSampleRate: 1.0,
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
